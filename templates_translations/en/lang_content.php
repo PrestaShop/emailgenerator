d37c8bf500ab8ab933a65cc4d9c3b10c21f7fa34
djfm <fm.de.jouvencel@gmail.com>
2014-01-23 16:12:25 +0000

    // cleanup

--- templates_translations/en/lang_content.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-global $_LANGMAIL;
-$_LANGMAIL = array();
-
-$_LANGMAIL[''] = 'Content-Type: text/plain; charset=UTF-8\nMIME-Version: 1.0\nContent-Transfer-Encoding: 8bit\n';
-
-
-return $_LANGMAIL;
